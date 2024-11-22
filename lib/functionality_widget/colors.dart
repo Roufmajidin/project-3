ced9c4d10807ebee726e903c624588d0f811f8d4
Rouf Majidin <roufmajidin777@gmail.com>
2024-11-23 00:50:34 +0700

    update and add asset

---
+++ lib/functionality_widget/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primary = Color(0xff0097b2); // Ungu
+  // static const Color primaryVariant = Color(0xFF3700B3);
+
+  // Secondary Colors
+  static const Color secondary = Color(0xFF03DAC6); // Hijau
+  static const Color secondaryVariant = Color(0xFF018786);
+
+  // Background Colors
+  static const Color background = Color(0xFFF5F5F5); // Abu-abu muda
+  static const Color surface = Color(0xFFFFFFFF); // Putih
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF212121); // Hitam gelap
+  static const Color textSecondary = Color(0xFF757575); // Abu-abu
+
+  // Error Colors
+  static const Color error = Color(0xFFB00020); // Merah
+
+  // Custom Colors
+  static const Color success = Color(0xFF4CAF50); // Hijau sukses
+  static const Color warning = Color(0xFFFFC107); // Kuning
+}
